ff5e86037685f5a99c68ec4ebc9ee727792b5fdc
Érico Borgonove <borgonove@gmail.com>
2023-04-04 10:57:12 -0400

    aula primeira tela

---
+++ PrimeiraTela/src/main/java/com/mycompany/primeiratela/PrimeiraTela.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primeiratela;
+
+/**
+ *
+ * @author erico
+ */
+public class PrimeiraTela {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
